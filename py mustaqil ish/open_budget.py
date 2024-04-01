3ae65a6f63d0d68bbc01c2dcd8fc07269afe9f91
MrBekhruz <mrbekhruzprogrammer@gmail.com>
2024-04-01 21:45:49 +0500

    new

--- py mustaqil ish/open_budget.py
+++ py mustaqil ish/open_budget.py
@@ -3,49 +3,59 @@
 # biror kuchaga ovoz berib quyilsin
 import json
 import random
-def open_budget():
-    malumot = []
+class Open:
+    def __init__(self,ism,familiya,telefon_raqam,pasport,idcha,data):
+        self.ism = ism
+        self.familiya = familiya
+        self.telefon_raqam = telefon_raqam
+        self.pasport = pasport
+        self.idcha = idcha
+        self.data = data
+
+
     while True:
-        print('----------------------')
-        print("----------Open budget dasturiga xush kelibsiz----------")
-        ism = input("Assalom alaykum ismingizni kiriting: ").lower()
-        familiya = input("Navbat endi familyaga, iltimos familiyangizni kiriting: ").lower()
-        telefon_raqam = input("Iltimos telefon raqamingizni kirirting \nNAMUNA -> (+998)99-414-40-22: ")
-        pasport = input('pasportseriya raqami: ')
-        id = random.choice('123456879')
-        for i in telefon_raqam:
-            if i == '-':
-                continue
-            print(i, end="")
-        data = {
-            'pasport':pasport,
+        def register(self):
+            print('----------------------')
+            print("----------Open budget dasturiga xush kelibsiz----------")
+            self.ism = input("Assalom alaykum ismingizni kiriting: ").lower()
+            self.familiya = input("Navbat endi familyaga, iltimos familiyangizni kiriting: ").lower()
+            self.telefon_raqam = input("Iltimos telefon raqamingizni kirirting \nNAMUNA -> (+998)99-414-40-22: ")
+            self.pasport = input('pasportseriya raqami: ')
+            self.idcha = random.choice('123456879')
+            for i in self.telefon_raqam:
+                if i == '-':
+                    continue
+                print(i, end="")
+        def append_data(self):
+            malumot = []
+            self.data = {
+                'pasport':self.pasport,
 
-            id :{
-            'ism':ism,
-            'familiya':familiya,
-            'telefon_raqam':telefon_raqam,           
+                self.idcha :{
+                'ism':self.ism,
+                'familiya':self.familiya,
+                'telefon_raqam':self.telefon_raqam,           
+                }
             }
-        }
-        malumot.append(data)
+            malumot.append(self.data)
         # malumot = tuple(malumot)
-        if data['pasport'] == pasport :
-            print('afsuski ruyhatdan utgansiz')
-            break
-        else:
-            qishloq = ['Mitan','Arabsaroy','Qalqon Ota','Vomitan','Duldur','Qiziltepa','Armijon']
-            print('--Qaysi qishloqqa ovoz berasiz?--')
-            ovoz = input('>>> ')
-
-            if ovoz in len(qishloq):
-                print("Saqlandi ")
-                data.update({'ovoz':ovoz})
-                json_file_name = 'data_file.json'
-                with open(json_file_name,'w') as json_f:
-                    json.dump(malumot,json_f,indent=7)
-                print(f"json formatga saqlandi {json_f}")
+        def main(self):
+            if self.data['pasport'] == self.pasport :
+                print('afsuski ruyhatdan utgansiz')
+                break
             else:
-                print('Ovozni xato berdingiz')
-open_budget()
-            
+                qishloq = ['Mitan','Arabsaroy','Qalqon Ota','Vomitan','Duldur','Qiziltepa','Armijon']
+                print('--Qaysi qishloqqa ovoz berasiz?--')
+                ovoz = input('>>> ')
 
-    
+                if ovoz in len(qishloq):
+                    print("Saqlandi ")
+                    self.data.update({'ovoz':ovoz})
+                    json_file_name = 'data_file.json'
+                    with open(json_file_name,'w') as json_f:
+                        json.dump(malumot,json_f,indent=7)
+                    print(f"json formatga saqlandi {json_f}")
+                else:
+                    print('Ovozni xato berdingiz')
+        if __name__ == '__main__':
+            
